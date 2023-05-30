9461924b7c6ee40fbab8df28199956260f54adbc
m21102001 <mohammedtarek21102001@gmail.com>
2023-05-30 15:06:14 +0300

    handel routes dom and added pages to test routes
    handel routes dom and added pages to test routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Tour from "./components/Tour";
+import Flight from "./components/Flight";
+import Review from "./components/Review";
+import Footer from "./components/Footer";
+import Forgetpass from "./pages/Forgetpass";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+      <Route path='/' element={
+          <>
+            <Navbar />
+            <Home />
+            <About />
+            <Tour />
+            <Flight />
+            <Review />
+            <Footer />
+          </>
+        } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgetpass" element={<Forgetpass/>}/>
+        
+      </Routes>
     </div>
   );
 }
