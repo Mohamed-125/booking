5ad25b0e01c031170f1b381922d2d29b0b2d119b
mohamed ibrahim <mohamedibrahim.fed@gmail.com>
2023-05-30 15:12:32 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import "./App.css";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+// import {
+//   createBroweserRouter,
+//   createRoutesFromElements,
+// } from "react-router-dom";
+import {
+  Outlet,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router";
+
+import { createBrowserRouter } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    )
+  );
+
   return (
-    <div className="App">
-      <Login />
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
+
+const Root = () => {
+  <div>
+    <Outlet />
+  </div>;
+};
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,51 +1,47 @@
 import React, { useRef, useState } from "react";
-
+import emailIcon from "../assests/email.png";
+import hideIcon from "../assests/hide.png";
+import { BsEye } from "react-icons/bs";
 const Login = () => {
   const passwordInputRef = useRef();
   const [showPassWord, setShowPassword] = useState(false);
   const showOrHidePassword = () => {
-    if (showPassWord) passwordInputRef.current.type = "text";
-    if (!showPassWord) passwordInputRef.current.type = "password";
+    setShowPassword((pre) => !pre);
+    if (showPassWord) {
+      passwordInputRef.current.type = "text";
+    } else {
+      passwordInputRef.current.type = "password";
+    }
   };
+
   return (
     <div className="form-container">
       <div className="form-div container">
         <div>
-          {" "}
           <h2>Login</h2>
           <div className="input-icons-div  ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="43"
-              height="43"
-              viewBox="0 0 43 43"
-            >
-              <image id="email" width="43" height="43" />
-            </svg>
+            <img src={emailIcon} />
             <input type="email" placeholder="heeeey" />
           </div>
           <div className="input-icons-div  ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="43"
-              height="43"
-              viewBox="0 0 43 43"
-            >
-              <image id="email" width="43" height="43" />
-            </svg>
+            <img src={emailIcon} />
             <input
               type="password"
               placeholder="heeeey"
               ref={passwordInputRef}
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="43"
-              height="43"
-              viewBox="0 0 43 43"
-            >
-              <image id="email" width="43" height="43" />
-            </svg>
+            {showPassWord ? (
+              <img
+                src={hideIcon}
+                style={{ cursor: "pointer" }}
+                onClick={showOrHidePassword}
+              />
+            ) : (
+              <BsEye
+                style={{ cursor: "pointer" }}
+                onClick={showOrHidePassword}
+              />
+            )}
           </div>
           <div></div>
           <button>Submit</button>
