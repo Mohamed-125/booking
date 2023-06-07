da256006a920afa789731e7f8716de37f0b35e14
mohamed ibrahim <mohamedibrahim.fed@gmail.com>
2023-06-08 02:27:06 +0300

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import FooterImg from "./image/footer.jpg"
-import "./Footer.css"
+import React from "react";
+import FooterImg from "./image/footer.jpg";
+import "./Footer.css";
 // import { a, Nava } from 'react-router-dom'
 
 const Footer = () => {
@@ -11,20 +11,52 @@ const Footer = () => {
         <div className="container">
           <footer className=" my-4">
             <ul className="nav d-flex justify-content-between border-bottom pb-3 mb-3">
-              <li className="nav-item" ><a href="#" className="nav-a px-2 text-muted">Home</a></li>
-              <li className="nav-item" ><a href="#about" className="nav-a px-2 text-muted">About</a></li>
-              <li className="nav-item" ><a href="#tour" className="nav-a px-2 text-muted">Tour</a></li>
-              <li className="nav-item" ><a href="#flight" className="nav-a px-2 text-muted">Flight</a></li>
-              <li className="nav-item" ><a href="#reviews" className="nav-a px-2 text-muted">Review</a></li>
-              <li className="nav-item" ><a href="#       " className="nav-a px-2 text-muted">Privacy Policy</a></li>
-              <li className="nav-item" ><a href="#       " className="nav-a px-2 text-muted">Terms & Condition</a></li>
-              <li className="nav-item" ><a href="#       " className="nav-a px-2 text-muted">Contact Us</a></li>
+              <li className="nav-item">
+                <a href="#" className="nav-a px-2">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#about" className="nav-a px-2">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#tour" className="nav-a px-2">
+                  Tour
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#flight" className="nav-a px-2">
+                  Flight
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#reviews" className="nav-a px-2">
+                  Review
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#       " className="nav-a px-2">
+                  Privacy Policy
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#       " className="nav-a px-2">
+                  Terms & Condition
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#       " className="nav-a px-2">
+                  Contact Us
+                </a>
+              </li>
             </ul>
           </footer>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
