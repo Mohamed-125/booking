b6f5cabc1ab38b05b958854307245cfb08d1937a
mohamed ibrahim <mohamedibrahim.fed@gmail.com>
2023-06-08 01:58:18 +0300

    bootstrap download

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./context/UserContext";
 import FlightDataFilterContext from "./context/FlightDataFilterContext.jsx";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
